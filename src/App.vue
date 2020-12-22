9a0d91d2d91f40c421dff9bfc58e20170fdde18b
Your Name <email@example.com>
2020-12-23 01:14:20 +0800

    7

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    APP
+    <Header></Header>
+    <router-view></router-view>
+    <Footer v-show="!$route.meta.isHideFooter"></Footer>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
- 
+ components:{
+   Header,
+   Footer
+ }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- 列表 -->
+    <ListContainer></ListContainer>
+    <!-- 今日推荐 -->
+    <TodayRecommend></TodayRecommend>
+    <!-- 商品排行 -->
+    <Rank></Rank>
+    <!-- 今日推荐 -->
+    <Like></Like>
+    <!-- 楼层 -->
+    <Floor v-for="floor in floors" :floor="floor" :key="floor.id"></Floor>
+    <!-- 商标 -->
+    <Brand></Brand>
+  </div>
+</template>
+
+<script>
+
+import ListContainer from './ListContainer/ListContainer'
+import TodayRecommend from './TodayRecommend/TodayRecommend'
+import Rank from './Rank/Rank'
+import Like from './Like/Like'
+import Floor from './Floor/Floor'
+import Brand from './Brand/Brand'
+
+export default {
+  name: 'Home',
+  
+  components:{
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
